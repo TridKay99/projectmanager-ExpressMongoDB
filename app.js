f58cf16574ff42325c5e4ab6fe5a562e1b62fe08
Tristan Kay <tridkay90@gmail.com>
2019-06-18 15:42:55 +1000

    all CRUD operations working

--- app.js
+++ app.js
@@ -21,10 +21,56 @@ app.listen(port, () => console.log(`listening on port ${port}...`));
 
 
 
-app.get('/', (req,res) => {
-  res.send('butts')
+app.get('/allProjects', (req, res)=>{
+  Project.find({})
+  .then( allProjects => {
+    return res.json(allProjects)
+  })
+  .catch (err => {
+    console.log(err);
+  })
 })
 
+app.get('/projects/:id', (req, res)=>{
+  const { id } = req.params
+  Project.find({id: id})
+  .then( project => {
+    return res.json(project)
+  })
+  .catch( err => {
+    return res.json(err)
+  });
+})
+
+
+app.put('/projects/:id', (req, res) => {
+  const { id } = req.params
+  const { newName } = req.body
+  Project.findOne({id})
+  .then( project => {
+    project.name = newName
+    project.save()
+    .then( (doc) => {
+      res.send(`${doc.name} has been updated`)
+    })
+    .catch( error => console.log(error) )
+  })
+  .catch( err => res.json(err))
+  })
+
+
+
+app.delete('/projects/:id', (req, res)=>{
+  const { id } = req.params
+  Project.findOneAndDelete({ id })
+  .then( doc => {
+    if(!doc) return res.send(`No project found at name ${name}`)
+    res.send(`${doc.name} deleted from database`)
+  })
+  .catch((err) => {
+    return res.json(err)
+  });
+})
 
 app.post('/newProject', (req,res) => {
   const { id, name } = req.body
@@ -33,6 +79,22 @@ app.post('/newProject', (req,res) => {
   .then( newProject => {
     console.log(newProject);
     res.json(newProject);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
+
+
+
+app.post('/project', (req, res) => {
+  // requires all of these in postman to post
+  const { id, name, moves, height } = req.body
+
+  Pokemon.create({ id, name, moves, height })
+  .then( newPoke => {
+    console.log(newPoke);
+    res.json(newPoke);
   })
   .catch((err) => {
     console.log(err);
